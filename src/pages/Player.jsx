778c72a253b28ff7787b9ac6c3432f16275e3408
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-04-30 16:19:42 +0530

    netflix Home completed

---
+++ src/pages/Player.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import { BsArrowLeft } from "react-icons/bs";
+
+const Player = () => {
+  return <div>Player</div>;
+};
+
+export default Player;
